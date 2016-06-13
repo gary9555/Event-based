b8959fdf15a0166b4b21c207e7096c795dc14df4
Gary Wenter <gary9555@gmail.com>
2016-06-13 15:52:28 +0200

    Update math.m

--- math.m
+++ math.m
@@ -1,9 +1,17 @@
-for k = 0.01:0.01:0.1
+figure(1);
+for id = 0.0078:0.0078:0.1
    x = 0.01:0.01:2.5;
-   y1 = sqrt(0.01*x.^2/k^2 - x.^2+k^2/4-0.05^2);    
-   y2 = sqrt(0.01/k^2-1)*x;
+   y1 = sqrt(0.01*x.^2/id^2 - x.^2+id^2/4-0.05^2);    
+   y2 = sqrt(0.01/id^2-1)*x;   
    plot(x,y1,'-r');
    hold on;
-   plot(x,y2,'-g');
+   plot(x,y2,'--g');
+   legend('Actual locus','Approximated locus','Location','NorthEast');
    
 end
+
+figure(2);
+id = 0.0078:0.0078:0.1;
+k = sqrt(0.01./(id.^2)-1);  % slope of the lines
+y3 = (pi/2-atan(k))/pi*180; % the itd angles 
+plot(id,y3);
