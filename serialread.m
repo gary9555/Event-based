2f300477a6f5b6d83010f1ff43bd0c1d4c904060
Gary Wenter <gary9555@gmail.com>
2016-06-13 15:53:11 +0200

    Update serialread.m

--- serialread.m
+++ serialread.m
@@ -1,4 +1,4 @@
-s = serial('COM4');	           % creating object for s COM port
+s = serial('COM5');	           % creating object for s COM port
 set(s, 'BaudRate',4000000, 'FlowControl','hardware');     % configuring the buad to 4000000, rest are set to default
 set(s, 'DataBits',8);
 set(s, 'OutputBufferSize',2048);
@@ -23,22 +23,25 @@ while(1)
     end
     if strcmp(cmd, 'a')
         fprintf(s, '!m0=%10');
-        fprintf(s, '!m1=%30');
+        fprintf(s, '!m1=%40');
         continue;
     end
     if strcmp(cmd, 'd')
-        fprintf(s, '!m0=%30');
-        fprintf(s, '!m1=%10');
+        fprintf(s, '!m0=%35');
+        fprintf(s, '!m1=%35');
         continue;
     end
     if strcmp(cmd, 's')
+        fprintf(s, '!m0=%-20');
+        fprintf(s, '!m1=%-20');
+        continue;
+    end
+    if strcmp(cmd, 't')
         fprintf(s, '!m0=%0');
         fprintf(s, '!m1=%0');
         continue;
-    end
-    
-    fprintf(s,cmd);    % write data to the port
-    
+    end    
+    fprintf(s,cmd);    % write data to the port    
     
 %     pause(0.1);
 %     out = fscanf(s);  % read from the port
